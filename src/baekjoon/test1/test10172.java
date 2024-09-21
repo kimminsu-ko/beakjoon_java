23a068b772f3c1621051db6f5b0b1b342dd52c4f
tkatnsdl1996@daum.net <tkatnsdl1996@daum.net>
2024-09-21 18:52:17 +0900

    2024.09.21 - 06:52

---
+++ src/baekjoon/test1/test10172.java
@@ -0,0 +1,17 @@
+package baekjoon.test1;
+
+//아래 예제와 같이 개를 출력하시오.
+//|\_/|
+//|q p|   /}
+//( 0 )"""\
+//|"^"`    |
+//||_/=\\__|
+public class test10172 {
+    public static void main(String[] args){
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.print("||_/=\\\\__|");
+    }
+}
---
+++ src/baekjoon/test1/test1330.java
@@ -0,0 +1,19 @@
+package baekjoon.test1;
+import java.util.Scanner;
+
+//두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+public class test1330 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if(A>B){
+            System.out.print(">");
+        } else if (A<B) {
+            System.out.print("<");
+        } else {
+            System.out.print("==");
+        }
+    }
+}
